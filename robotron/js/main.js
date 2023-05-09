7305c22ca8cb8d2af6d46946c13c833eb90e09b7
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-05-09 17:06:55 -0300

    montando uma peça

--- robotron/js/main.js
+++ robotron/js/main.js
@@ -1,4 +1,17 @@
 const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+//pegar o conteúdo
+controle.forEach( (elemento) => { 
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent)
+    }) 
+})
+
 robotron.addEventListener("click",(evento) =>{ //function anonima, arrow function
     console.log(evento)
 })
@@ -9,3 +22,11 @@ function dizOi(nome){
 }
 
 dizOi("pedro")
+
+function manipulaDados(operacao){
+    if(operacao === "-"){
+        braco.value = parseInt(braco.value) - 1
+    }else{
+        braco.value = parseInt(braco.value) + 1
+    }
+}

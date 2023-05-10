98b5b3435b7b61344c4cd2198aaa0e5ee31635f7
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-05-10 07:20:52 -0300

    conclusão

--- robotron/js/main.js
+++ robotron/js/main.js
@@ -1,32 +1,59 @@
-const robotron = document.querySelector("#robotron")
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-
 const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
 
-//pegar o conteúdo
-controle.forEach( (elemento) => { 
-    elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
-    }) 
-})
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-robotron.addEventListener("click",(evento) =>{ //function anonima, arrow function
-    console.log(evento)
-})
-
-function dizOi(nome){
-    console.log("oi"+ nome)
-    console.log("bem-vindo ao robotron 2000")
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
 }
 
-dizOi("pedro")
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
+    })
+})
 
-function manipulaDados(operacao, controle){
+function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
-    if(operacao === "-"){
-        peca.value = parseInt(peca.value) - 1
-    }else{
+
+    if(operacao === "-") {
+        peca.value = parseInd(peca.value) - 1
+    } else {
         peca.value = parseInt(peca.value) + 1
     }
+}
+
+function atualizaEstatistica(peca) {
+    estatistica.forEach( (elemento ) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
